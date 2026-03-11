463c51622f30f9886f5477214b4173089ff64de0
nookjoook56-web <nookjoook56@gmail.com>
2026-03-11 06:20:23 +0300

    Update updater.py

--- updater.py
+++ updater.py
@@ -1,20 +1,19 @@
 import requests
-import os
 
 def get_vavoo_signature():
     url = "https://vavoo.to/config"
     headers = {"User-Agent": "VAVOO/2.6"}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        # Vavoo'nun o anki aktif anahtarını/imzasını ayıkla
-        config_data = response.json()
-        signature = config_data.get("signature")
-        return signature
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.json().get("signature")
+    except:
+        return None
     return None
 
-# Aldığımız imzayı bir dosyaya yazıyoruz (veya FastAPI sunucuna yolluyoruz)
 sig = get_vavoo_signature()
 if sig:
-    print(f"Yeni İmza Alındı: {sig}")
-    # Burada imzayı GitHub repona push edebilir veya bir database'e yazabilirsin
-  
+    with open("signature.txt", "w") as f:
+        f.write(sig)
+    print("İmza başarıyla signature.txt dosyasına yazıldı.")
+    
